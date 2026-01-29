e26ae5b0cb7b6547122b1cb0bf769a0a391c54cb
PhamMinhKha0710 <PhamMinhKha19091710@gmail.com>
2026-01-29 17:09:20 +0700

    feat(db): add migration for Social Networking entities

--- backend/src/DevComunity.Application/Common/DTOs/PaginatedResponse.cs
+++ backend/src/DevComunity.Application/Common/DTOs/PaginatedResponse.cs
@@ -5,7 +5,7 @@ namespace DevComunity.Application.Common.DTOs;
 /// </summary>
 public class PaginatedResponse<T>
 {
-    public List<T> Items { get; set; } = new();
+    public IEnumerable<T> Items { get; set; } = new List<T>();
     public int TotalCount { get; set; }
     public int Page { get; set; }
     public int PageSize { get; set; }
