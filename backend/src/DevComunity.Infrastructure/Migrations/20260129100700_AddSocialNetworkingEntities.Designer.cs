4f102a5ec3ad02283a4efdd0e936b67a097071fc
PhamMinhKha0710 <PhamMinhKha19091710@gmail.com>
2026-01-29 17:09:20 +0700

    feat(db): add migration for Social Networking entities

--- backend/src/DevComunity.Infrastructure/Migrations/20260129100700_AddSocialNetworkingEntities.Designer.cs
+++ backend/src/DevComunity.Infrastructure/Migrations/20260129100700_AddSocialNetworkingEntities.Designer.cs
@@ -4,6 +4,7 @@ using DevComunity.Infrastructure.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DevComunity.Infrastructure.Migrations
 {
     [DbContext(typeof(DevComunityDbContext))]
-    partial class DevComunityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260129100700_AddSocialNetworkingEntities")]
+    partial class AddSocialNetworkingEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
