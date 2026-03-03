d3c262f5ff8a2c8f6e9fe3c22fea34f620dd52c6
PhamMinhKha0710 <phamminhkha19091710@gmail.com>
2026-03-03 20:41:23 +0700

    optimize(infra): add IMemoryCache with CacheService for tags and hot data caching

---
+++ backend/src/SocialTechsy.SocialNetwork.Application/Interfaces/Services/ICacheService.cs
@@ -0,0 +1,8 @@
+namespace SocialTechsy.SocialNetwork.Application.Interfaces.Services;
+
+public interface ICacheService
+{
+    Task<T?> GetOrCreateAsync<T>(string key, Func<Task<T>> factory, TimeSpan? expiration = null);
+    void Remove(string key);
+    void RemoveByPrefix(string prefix);
+}
